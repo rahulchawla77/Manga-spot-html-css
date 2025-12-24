8d8c0f99eba1b7396d0afa1679ea94bd0fa69e20
rahul chawla <rahulchawla5012@gmail.com>
2025-12-24 11:42:32 +0530

    Initial static version (HTML & CSS)

---
+++ javascript/data.js
@@ -0,0 +1,23 @@
+const mangas = [
+  {
+    id: "one-piece",
+    title: "One Piece",
+    image: "onepiece.jpg",
+    description: "The greatest story ever told.",
+    chapters: ["Chapter 1", "Chapter 2"]
+  },
+  {
+    id: "naruto",
+    title: "Naruto",
+    image: "naruto.jpg",
+    description: "A ninja who wants to be Hokage.",
+    chapters: ["Chapter 1", "Chapter 2"]
+  },
+  {
+    id: "bleach",
+    title: "Bleach",
+    image: "bleach.jpg",
+    description: "A soul reaper’s journey.",
+    chapters: ["Chapter 1", "Chapter 2"]
+  }
+];
